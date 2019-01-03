8b0830e19707a540458723380a9a4e410aa44d25
Vladius25 <vkorol2509@icloud.com>
2019-01-03 20:01:15 +0500

    Template for ui

--- run.py
+++ run.py
@@ -1,54 +1,16 @@
-import os
-
-from src.blue import BlueConnector
-from src.media import MediaController
-import subprocess as sp
+import sys
+from PyQt5 import QtWidgets
+import src.gui
 
 
-def init():
-    global controller, connector
-    connector = BlueConnector()
-    device = connector.connect()
-
-    controller = MediaController(device)
+class BlueApp(QtWidgets.QMainWindow, src.gui.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
-def execute(ch):
-    if ch == '1':
-        controller.play()
-    elif ch == '2':
-        controller.stop()
-    elif ch == '3':
-        controller.next()
-    elif ch == '4':
-        controller.prevoius()
-    elif ch == '5':
-        print(str(connector.state))
-    elif ch == 'q':
-        os._exit(0)
-    else:
-        pass
-
-
-def run():
-    sp.call('clear')
-    print("Connected")
-
-    ch = ''
-    while ch != 'q':
-        print("What you want to do?")
-        print("1) Play")
-        print("2) Stop")
-        print("3) Next")
-        print("4) Prev")
-        print("5) Now playing")
-        print("q) Exit")
-
-        ch = input("Choose: ")
-        sp.call('clear', shell=True)
-        execute(ch)
-
-
-if __name__ == "__main__":
-    init()
-    run()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = BlueApp()
+    window.show()
+    app.exec_()
